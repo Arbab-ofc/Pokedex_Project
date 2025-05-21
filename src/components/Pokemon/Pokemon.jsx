f762d6256c3aaae0082bbbcfa9c84da1db4b0d44
arbab arshad <arshadsarhan786@gmail.com>
2025-05-21 11:11:40 +0530

    Basic Pokedex Setup Done

---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,12 @@
+import './Pokemon.css'
+
+function Pokemon( { name , image } ) {
+    return (
+        <div>
+            <div>{name}</div>
+            <div><img src={image} /></div>
+       </div>
+    )
+}
+
+export default Pokemon;
